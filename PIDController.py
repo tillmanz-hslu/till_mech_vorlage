92d2dd3e4aff3445ec6bb3aad343be99485d1300
Till Manz <till.manz@stud.hslu.ch>
2026-05-19 14:14:25 +0200

    anti WIndup implementiert

--- PIDController.py
+++ PIDController.py
@@ -2,7 +2,6 @@
 # Author: Simon van Hemert <simon.vanhemert@hslu.ch>
 # Author: Peter Sollberger <peter.sollberger@hslu.ch>
 
-
 class PIDController:
     """
     Implements a PID controller.
@@ -28,22 +27,39 @@ class PIDController:
         self.error_integral = 0
 
     def calculate_controller_output(self, actual_value):
+
         """
         Calculate next target values with the help of a PID controller.
         """
-        sampling_time = 0.01
+        #  1. Speichern Sie den vorherigen Fehler in der Variablen
+        #     'error_linear_old', berechnen Sie den neuen Fehler und
+        #     speichern Sie diesen in self.error_linear
         error_linear_old = self.error_linear
+        # TODO: Implementieren
+        #  2. Berechnen Sie
+        #     - den aktuellen Positions-Fehler 'self.error_linear'
+        #     - das aktuelle Fehler-Integral 'self.error_integral'; denken
+        #       Sie dabei an windup
+        #     - das aktuelle Fehler-Derivative 'error_derivative'
         self.error_linear = self.reference_value - actual_value
+        self.error_integral += self.error_linear * 0.01
 
-        self.error_integral += self.error_linear * sampling_time
-        integral_limit = self.anti_windup / (self.kp / self.Tn)
-        self.error_integral = max(min(self.error_integral, integral_limit), -integral_limit)
+        if self.error_integral * self.kp / self.Tn > self.anti_windup:
+            self.error_integral = self.anti_windup * self.Tn / self.kp
+        elif self.error_integral * self.kp / self.Tn < -self.anti_windup:
+            self.error_integral = -self.anti_windup * self.Tn / self.kp
 
-        error_derivative = (self.error_linear - error_linear_old) / sampling_time
+        error_derivative = (self.error_linear - error_linear_old) / 0.01
 
+        # TODO: Implementieren
+        #  3. Berechnen Sie aus den Fehlern die P, I und D-Anteile;
+        #     Sie können diese Werte in den Variablen p_part, i_part
+        #     und d_part abspeichern oder die Berechnungen direkt in die
+        #     Liste der pid_actions schreiben
         p_part = self.kp * self.error_linear
         i_part = self.kp / self.Tn * self.error_integral
         d_part = self.kp * self.Tv * error_derivative
+        # TODO: Implementieren
 
         # Save the three parts of the controller in a vector
         pid_actions = [p_part, i_part, d_part]
